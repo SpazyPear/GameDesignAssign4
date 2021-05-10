56040535eee17f76077b7b4248a2e4272847be8d
maxhey <81597462+maxhey@users.noreply.github.com>
2021-05-10 20:29:38 +1000

    added slime animation

--- Assets/Scripts/Enemies/PatrollingWallToWall.cs
+++ Assets/Scripts/Enemies/PatrollingWallToWall.cs
@@ -8,17 +8,26 @@ public class PatrollingWallToWall : MonoBehaviour
     public bool enemyIsGoingRight = true;
     public float raycastingDistance = 1f;
 
+    public int mAttackDamage = 2;
+    public float timer = 5;
+
     private SpriteRenderer _mSpriteRenderer;
-   
+
+    [SerializeField] Animator enemyAnim;
+
     void Start()
     {
         _mSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         _mSpriteRenderer.flipX = enemyIsGoingRight;
+
+        enemyAnim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
+
         //if enemy goes right get vector pointing right
         Vector3 directionTranslation = (enemyIsGoingRight) ? transform.right : -transform.right;
         directionTranslation *= Time.deltaTime * enemyMovementSpeed;
@@ -26,6 +35,11 @@ public class PatrollingWallToWall : MonoBehaviour
         transform.Translate(directionTranslation);
 
         CheckForWalls();
+
+        if (timer == 0)
+        {
+            //attack
+        }
     }
 
     private void CheckForWalls()
@@ -34,9 +48,9 @@ public class PatrollingWallToWall : MonoBehaviour
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position + raycastDirection * raycastingDistance - new Vector3(0f, 0.25f, 0f), raycastDirection, 0.075f);
 
-        if (hit.collider != null) 
+        if (hit.collider != null)
         {
-            if (hit.transform.tag == "Wall")
+            if (hit.transform.tag == "Wall" || hit.transform.tag == "Enemy")
             {
                 enemyIsGoingRight = !enemyIsGoingRight;
                 _mSpriteRenderer.flipX = enemyIsGoingRight;
