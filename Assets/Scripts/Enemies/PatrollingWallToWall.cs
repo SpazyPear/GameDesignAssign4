148b2f2bfafe0cbae333159d65a167ae09f7d9fe
maxhey <81597462+maxhey@users.noreply.github.com>
2021-05-17 21:52:34 +1000

    fixed slime enemy script

--- Assets/Scripts/Enemies/PatrollingWallToWall.cs
+++ Assets/Scripts/Enemies/PatrollingWallToWall.cs
@@ -50,7 +50,7 @@ public class PatrollingWallToWall : MonoBehaviour
 
         if (hit.collider != null)
         {
-            if (hit.transform.tag == "Ground"  || hit.transform.tag == "Enemy" || hit.transform.tag == "Hazard" )
+            if (hit.transform.tag == "Ground" || hit.transform.tag == "Enemy" || hit.transform.tag == "Hazard" || hit.transform.tag == "Wall" )
             {
                 enemyIsGoingRight = !enemyIsGoingRight;
                 _mSpriteRenderer.flipX = enemyIsGoingRight;
