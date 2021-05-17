76e7903e79712da586a9d3d76e17977171693738
kin6LS <nonac201@gmail.com>
2021-05-17 23:28:57 +1000

    REdesign Level 5

--- Assets/Scripts/Level 5/PathWay.cs
+++ Assets/Scripts/Level 5/PathWay.cs
@@ -24,7 +24,7 @@ public class PathWay : MonoBehaviour
     {
         if (button.pressed)
         {
-                GetComponent<Collider2D>().enabled = !isCollider;
+                GetComponent<TilemapCollider2D>().enabled = !isCollider;
                 GetComponent<TilemapRenderer>().enabled = !isVisable;
         }
     }
