2619e76409ea98f2abf1a2dc18da193f21a5469b
Zelnonmon <13886801@users.noreply.github.com>
2021-05-18 10:32:16 +1000

    Fixed Kin's ending scene

--- Assets/Scripts/Ending/ending.cs
+++ Assets/Scripts/Ending/ending.cs
@@ -1,27 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class ending : MonoBehaviour
 {
     public float timeRemaining = 5;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if(timeRemaining > 0)
+        if((timeRemaining -= Time.deltaTime) < 0)
         {
-            timeRemaining -= Time.deltaTime;
-        }
-        else
-        {
-            SceneManager.LoadScene("Title Screen");
+            GameObject.FindGameObjectWithTag("Level Manager").GetComponent<LevelManager>().LoadSceneByIndex(0);
+            Destroy(gameObject);
         }
     }
 }
