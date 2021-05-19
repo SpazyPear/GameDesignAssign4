0afbeb86ec03ab6bbd38bcd8ad792a33f1381144
Zelnonmon <13886801@users.noreply.github.com>
2021-05-19 11:10:30 +1000

    Got rid of pointless code

--- Assets/Scripts/MC/PlayerControls.cs
+++ Assets/Scripts/MC/PlayerControls.cs
@@ -48,16 +48,13 @@ public class PlayerControls : MonoBehaviour
         {
             DoMouseClick();
         }
+
+        anim.SetBool("isClimbing", false);
         if (Input.GetKey(KeyCode.W))
         {
             anim.SetBool("isClimbing", canClimb);
             rb.velocity = new Vector2(rb.velocity.x, (canClimb) ? climbSpeed * Input.GetAxisRaw("Vertical") : rb.velocity.y);
         }
-        else
-        {
-            anim.SetBool("isClimbing", false);
-        }
-        
     }
 
     private void DoMouseClick()
